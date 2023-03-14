a6a4f4a90045e07fee0663ab9a031c93f697541f
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2023-03-14 08:33:19 +0100

    début de la création de la page d'accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'Maison Des Ligues')]
+class AccueilController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
