e60384b86a60840e8cfc39b3410cdced8d46de0d
Ancestra11 <AdrienD.pro@yahoo.com>
2023-03-18 16:22:30 +0100

    Début de la création du formulaire de création d'un nouveau compte utilisateur

---
+++ resources/SymfonyInstallDependencies.bat
@@ -0,0 +1,12 @@
+symfony composer require annotations
+symfony composer require --dev symfony/maker-bundle
+symfony composer require symfony/apache-pack --dev
+symfony composer require twig
+symfony composer require logger
+symfony composer require profiler --dev
+symfony composer require debug --dev
+symfony composer require symfony/orm-pack
+symfony composer require symfony/form 
+symfony composer require symfony/security-bundle 
+symfony composer require symfony/validator
+symfony composer require symfonycasts/verify-email-bundle symfony/mailer
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,31 +4,36 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(name:'id')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(name:'email', length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name:'roles')]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
-    #[ORM\Column]
+    #[ORM\Column(name:'password')]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(name:'numlicence', length: 255)]
     private ?string $numlicence = null;
+
+    #[ORM\Column(name:'isVerified', type: 'boolean')]
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -127,6 +132,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setNumlicence(string $numlicence): self
     {
         $this->numlicence = $numlicence;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
