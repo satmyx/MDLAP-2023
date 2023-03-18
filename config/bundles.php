2cd5885819e6810698d8d67e7ef8e51cd578e6b3
Satmyx <35663463+satmyx@users.noreply.github.com>
2023-03-18 14:18:42 +0100

    Entity User
    Création de l'entity User
    Identitfiant unique = Email

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
