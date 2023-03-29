add31d07bb3ff35423afae6206cbd2ef2f588549
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2023-03-29 16:34:01 +0200

    commit push

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -12,6 +12,7 @@ class AccueilController extends AbstractController
     #[Route('/')]
     public function index(): Response
     {
+        $resto="jrnfseu";        
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
