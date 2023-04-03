b5490c6e9f86bfb8d36aaa7f5367a6758bde4c1e
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2023-04-03 12:42:01 +0200

    push

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,23 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Atelier;
 use App\Repository\AtelierRepository;
 use App\Repository\ThemeRepository;
 use App\Repository\VacationRepository;
 use App\Repository\HotelRepository;
 //use Doctrine\ORM\EntityManagerInterface;
-//use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 #[Route('/', name: 'Maison Des Ligues')]
 class AccueilController extends AbstractController
 {
     #[Route('/')]
     public function index(AtelierRepository $atelierRepo, ThemeRepository $themeRepo, VacationRepository $vacRepo,
-            HotelRepository $hotelRepo/*, ManagerRegitry $doctrine*/): Response
+            HotelRepository $hotelRepo, ManagerRegistry $doctrine): Response
     {
         $datecongres = 'uneDate';
 //        $ateliers = $doctrine->getRepository(Atelier::class)->findAteliers($doctrine);
-//        $ateliers = $atelierRepo->findAll();
+        $ateliers= $atelierRepo->findAll(); //->findAll();
+        dump($ateliers);
 //        $ateliersNom = $ateliers->getLibelle();
 //        $ateliersNbPlaces = $ateliers->getNbPlaces();
         $themes = $themeRepo->findAll();
@@ -31,7 +33,7 @@ class AccueilController extends AbstractController
         
         return $this->render('accueil/index.html.twig', [
             'dateCongres' => $datecongres,
-//            'ateliers'=> $ateliers,
+            'ateliers'=> $ateliers,
 //            'ateliersNom'=> $ateliersNom,
 //            'ateliersPlaces'=> $ateliersNbPlaces,
             'themes' => $themes,
