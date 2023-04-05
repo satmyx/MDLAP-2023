06bde2369ac2365f96e561c716d245c9523cf5a1
Satmyx <dylanesculier@gmail.com>
2023-04-05 16:45:57 +0100

    Changement style

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -75,7 +75,7 @@ class RegistrationController extends AbstractController
                 (new TemplatedEmail())
                     ->from(new Address('mailer@mailer.de', 'mailer boot'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Maison des ligues - Confirmer votre email !')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
